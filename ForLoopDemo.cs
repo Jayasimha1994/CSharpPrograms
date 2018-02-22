7950f09bd0ee97a5a7751c126845200411106cc6
jayasimha <jayasimhajay1994@gmail.com>
2018-02-22 13:28:59 +0530

    Loops

---
+++ ForLoopDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class ForLoopDemo
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 20, 30, 40 };
+            Console.WriteLine("for loop");
+            for(int i=0;i<5;i++)
+            {
+                Console.WriteLine("For Loop Iteration number: " + i);
+            }
+            Console.WriteLine("forEach loop");
+            foreach(int i in arr)
+            {
+                Console.WriteLine("ForEach Iteration value: " + i);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ WhileLoopDemo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class WhileLoopDemo
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            Console.WriteLine("while loop");
+            while (i < 5)
+            {
+                Console.WriteLine("while loop iteration: " + i);
+                i++;
+            }
+            Console.WriteLine("do-while loop");
+            do
+            {
+                Console.WriteLine("do-while iteration: " + i);
+                i++;
+            }
+            while (i < 10);
+            Console.Read();
+        }
+    }
+}
