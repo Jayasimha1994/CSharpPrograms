06f620763d35f130571b735c5f25c371d29e59a7
jayasimha <jayasimhajay1994@gmail.com>
2018-02-26 19:03:02 +0530

    File IO

---
+++ BinaryWriterDemo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class BinaryWriterDemo
+    {
+        static void Main(string[] args)
+        {
+            BinaryWriter b = new BinaryWriter(File.Open("F:\\abc.txt", FileMode.OpenOrCreate));
+            b.Write("biz");
+            b.Write("@");
+            b.Write("123");
+            b.Write(".com");
+            b.Flush();
+            b.Close();
+            Console.Read();
+        }
+    }
+}
